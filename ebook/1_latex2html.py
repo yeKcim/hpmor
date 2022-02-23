7d9782e6a0a51286b4534a59de02934f523f278b
yeKcim <yekcim@crocobox.org>
2022-02-23 11:44:01 +0100

    vectorial Deathly_Hallows_Sign

--- ebook/1_latex2html.py
+++ ebook/1_latex2html.py
@@ -217,7 +217,7 @@ def tex2html(s: str) -> str:
         flags=re.DOTALL | re.IGNORECASE,
     )
 
-    was = "{\n\\begin{center}\n\\includegraphics[scale=0.125]{Deathly_Hallows_Sign.png}\n\\end{center}\n}"
+    was = "{\n\\begin{center}\n\\includegraphics[scale=0.75]{Deathly_Hallows_Sign.pdf}\n\\end{center}\n}"
     s = s.replace(was, "")
 
     # paper notes in Chapter 13
