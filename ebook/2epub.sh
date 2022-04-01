791d93aa3866a946a1cb9fe8b0dc4e2dbcb9c57e
yeKcim <yekcim@crocobox.org>
2022-04-01 07:08:40 +0200

    add some pandoc options in epub script

--- ebook/2epub.sh
+++ ebook/2epub.sh
@@ -1,5 +1,15 @@
 #/bin/bash
-latexpand hpmor.tex -o hpmor_flatten.tex																							# flatten tex
-sed -i '1i\\\input{hp-header}\n' hpmor_flatten.tex																					# + header pour les def \newcommand
-sed -i '/\\begin{headlines}/d ; /\\end{headlines}/d' hpmor_flatten.tex																# - \headlines (can't generate epub)
-pandoc -s hpmor_flatten.tex -o hpmor.epub -f latex+latex_macros --metadata title="Harry Potter et les méthodes de la Rationalité"	# tex to epub
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR/.."
+latexpand hpmor.tex -o hpmor_flatten.tex									# flatten tex
+sed -i '1i\\\input{hp-header}\n' hpmor_flatten.tex							# + header pour les def \newcommand
+sed -i '/\\begin{headlines}/d ; /\\end{headlines}/d' hpmor_flatten.tex		# - \headlines (can't generate epub)
+pdftoppm -png -singlefile hpmor.pdf cover									# create cover from pdf
+pandoc -s hpmor_flatten.tex -o hpmor.epub \									# tex to epub
+   -f latex+latex_macros \														# convert \newcommand
+   --metadata title="Harry Potter et les méthodes de la Rationalité" \			# title
+   --epub-cover-image="cover.png" \												# cover png
+   --epub-chapter-level=2														# new chapter each h2
+rm cover.png																# remove tmp files
+
+
