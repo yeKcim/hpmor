d34963aa9f8c8ea069da699c10d387d3ae4b6bc5
Torben <59419684+entorb@users.noreply.github.com>
2022-05-08 09:05:30 +0200

    annotations etc

---
+++ make_ebooks.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+bash ebook/2epub.sh
---
+++ make_pdfs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+latexmk hpmor
+
+# TODO
+# latexmk hpmor-1
+# latexmk hpmor-2
+# latexmk hpmor-3
+# latexmk hpmor-4
+# latexmk hpmor-5
+# latexmk hpmor-6
